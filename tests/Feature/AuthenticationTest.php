8257ad22266d63e5d0cc1ad3e2df88aab89bd1a7
Jed <jedtony.jt@gmail.com>
2022-08-06 04:41:06 +0100

    Add authentication tests

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class AuthenticationTest extends TestCase
@@ -37,5 +39,64 @@ class AuthenticationTest extends TestCase
 
         $this->json('POST', 'api/v1/register', $userData, ['Accept' => 'application/json'])
             ->assertStatus(201);
+    }
+
+     /**
+     * Test that a user cannot register without an email.
+     *
+     * @return void
+     */
+    public function testEmailIsRequiredForRegistration()
+    {
+        $userData = [
+            "first_name" => "John",
+            "last_name" => "Doe",
+            "password" => "demo12345",
+        ];
+        $this->json('POST', 'api/v1/register', $userData, ['Accept' => 'application/json'])
+            ->assertStatus(422);
+    }
+
+    /**
+     * Test that a user can login.
+     *
+     * @return void
+     */
+    public function testSuccessfulLogin()
+    {
+        $userData = [
+            "email" => "doe@example.com",
+            "password" => "demo12345",
+        ];
+
+        $this->json('POST', 'api/v1/login', $userData, ['Accept' => 'application/json'])
+            ->assertStatus(200);
+    }
+
+    /**
+     * Test that authenticated routes are protected.
+     *
+     * @return void
+     */
+    public function testAuthenticatedRoutesAreProtected()
+    {
+        $this->json('GET', 'api/v1/user', [], ['Accept' => 'application/json'])
+            ->assertStatus(401);
+    }
+
+    /**
+     * Test that authenticated users can access protected route
+     *
+     * @return void
+     */
+    public function testAuthenticatedUsersCanAccessProtectedRoute()
+    {
+        Sanctum::actingAs(
+            User::factory()->create(),
+            ['*']
+        );
+
+        $this->json('GET', 'api/v1/user', [], ['Accept' => 'application/json'])
+            ->assertStatus(200);
     }
 }
