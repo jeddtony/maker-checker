07b41f8297b8c4e98046739c0fb55409560235c6
Jed <jedtony.jt@gmail.com>
2022-08-06 12:50:31 +0100

    Add methods for pending update feature

---
+++ app/Http/Controllers/PendingRequestController.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PendingRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use App\Models\User;
+
+class PendingRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $pendingRequest = PendingRequest::where('status', 'pending')->get();
+
+        return $this->formatSuccessResponse('Pending requests', $pendingRequest);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created users in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+      
+        if ($validator->fails()) {
+            return $this->formatInputErrorResponse($validator->errors()->first());
+        }
+
+        $validatedData = $request->all();
+        $validatedData['password'] = bcrypt($request->password);
+        $validatedData['uuid'] = Str::uuid();
+        $validatedData['request_type'] = 'create';
+        $validatedData['admin_uuid'] = request()->user()->uuid;
+
+        $pendingRequest = PendingRequest::create($validatedData);
+
+        return $this->formatCreatedResponse('User creation request awaiting approval', $pendingRequest);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  $uuid
+     * @return \Illuminate\Http\Response
+     */
+    public function show($uuid)
+    {
+        //
+        $pendingRequest = PendingRequest::where('uuid', $uuid)->first();
+        if (!$pendingRequest) {
+            return $this->notFoundResponse('User not found');
+        }
+        return $this->formatSuccessResponse('User found', $pendingRequest);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PendingRequest  $pendingRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PendingRequest $pendingRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  User $uuid
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,  $uuid)
+    {
+        //
+        $user = User::where('uuid', $uuid)->first();
+
+        if (!$user) {
+            return $this->notFoundResponse('User you are trying to update does not exist');
+        }
+
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,'.$user->id,
+        ]);
+
+        if ($validator->fails()) {
+            return $this->formatInputErrorResponse($validator->errors()->first());
+        }
+
+        $validatedData = $request->all();
+        $validatedData['request_type'] = 'update';
+        $validatedData['uuid'] = Str::uuid();
+        $validatedData['admin_uuid'] = request()->user()->uuid;
+        $validatedData['user_uuid'] = $user->uuid;
+
+        $pendingRequest = PendingRequest::create($validatedData);
+        return $this->formatCreatedResponse('User update awaiting approval', $pendingRequest);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $uuid
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy( $uuid)
+    {
+        //
+        $user = User::where('uuid', $uuid)->first();
+
+        if (!$user) {
+            return $this->notFoundResponse('User you are trying to delete does not exist');
+        }
+        
+        $pending = PendingRequest::create([
+            'first_name' => $user->first_name,
+            'last_name' => $user->last_name,
+            'email' => $user->email,
+            'uuid' => Str::uuid(),
+            'request_type' => 'delete',
+            'admin_uuid' => request()->user()->uuid,
+            'user_uuid' => $user->uuid,
+        ]);
+
+        // Todo:: when a delete is approved the user uuid should be marked as a soft delete
+
+        return $this->formatSuccessResponse('User deletion request awaiting approval', $pending);
+        
+    }
+}
