a29a9a39554dcc2f8a1eace0efd49851d402858e
Jed <jedtony.jt@gmail.com>
2022-08-07 20:21:17 +0100

    Add seeder for admin and users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+
+        \App\Models\User::create([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'admin_one@mail.com',
+            'password'=> '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'uuid' => Str::uuid(),
+            'is_admin' => true,
+        ]);
+
+        \App\Models\User::create([
+            'first_name' => 'Mark',
+            'last_name' => 'White',
+            'email' => 'admin_two@mail.com',
+            'password'=> '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'uuid' => Str::uuid(),
+            'is_admin' => true,
+        ]);
     }
 }
